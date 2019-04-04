27a2273a9de0ca65449127120a2f54990b42cc92
Katie Moosman <moosman.katie@gmail.com>
2019-04-04 08:16:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts
   resources :categories
 end
